ef2a179cc9f6fc6408243ba463d4c8e40e78f4f6
Tristan-alpha <dazhou_liu2023@163.com>
2025-03-23 12:04:24 +0800

    to expand the dataset and call apis

--- evaluation.sh
+++ evaluation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# python /home/dazhou/ReasonEval/t-codes/evaluate_temperature_results.py --temperatures 0.8 1.8 --models Abel-7B-002 --gpu 2
+# python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --temperatures 0.1 0.2 0.4 0.5 0.7 0.9 1.1 1.2 1.4 1.5 1.7 1.9 --models Abel-7B-002 --gpu 0 --dataset_name mr-gsm8k
 
-# python /home/dazhou/ReasonEval/t-codes/evaluate_temperature_results.py --temperatures 1.0 1.3 --models Abel-7B-002 --gpu 4
+# python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --temperatures 1.0 1.3 --models Abel-7B-002 --gpu 4
 
-python /home/dazhou/ReasonEval/t-codes/evaluate_temperature_results.py --temperatures 1.8 --models WizardMath-7B-V1.1 --gpu 4
+python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --temperatures 0.1 0.2 0.4 0.5 0.7 0.9 1.1 1.2 1.4 1.5 1.7 1.9 --models WizardMath-7B-V1.1 --gpu 1 --dataset_name mr-gsm8k
