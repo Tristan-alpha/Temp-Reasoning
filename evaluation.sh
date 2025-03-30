ae5638c1726f62351a39f9bfad9267b03615ed74
dazhou from a800 <dazhou_liu2023@outlook.com>
2025-03-30 22:00:12 +0800

    merge

--- evaluation.sh
+++ evaluation.sh
@@ -3,4 +3,11 @@
 
 # python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --temperatures 1.0 1.3 --models Abel-7B-002 --gpu 4
 
-python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --temperatures 0.1 0.2 0.4 0.5 0.7 0.9 1.1 1.2 1.4 1.5 1.7 1.9 --models WizardMath-7B-V1.1 --gpu 1 --dataset_name mr-gsm8k
+# python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --temperatures 0.1 0.2 0.4 0.5 0.7 0.9 1.1 1.2 1.4 1.5 1.7 1.9 --models WizardMath-7B-V1.1 --gpu 1 --dataset_name mr-gsm8k
+gpu=7
+model="WizardMath-7B-V1.1"
+DATASET="hybrid_reasoning"
+temp_group="0.0 0.2"
+MODEL_SIZE="34B"
+REASONEVAL_PATH="GAIR/ReasonEval-34B"
+python /home/dazhou/ReasonEval/t-codes/evaluate_results.py --gpu $gpu --models \"$model\" --dataset_name $DATASET --temperatures $temperature_group --model_size $MODEL_SIZE --reasoneval_path $REASONEVAL_PATH
