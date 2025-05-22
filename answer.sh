ac7d746dc8770fb311b2a13656173812d6972693
Tristan-alpha <dazhou_liu2023@163.com>
2025-05-22 09:24:22 +0800

    update result

--- answer.sh
+++ answer.sh
@@ -5,12 +5,13 @@ SCRIPT_PATH="/home/dazhou/ReasonEval/t-codes/answer_generation.py"
 MODEL="Qwen3-32B"  # Specify the model you want to use
 DATASET="aime"
 SUBSET_SIZE=0
-TEMPERATURES=(0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 0.0 0.1 0.2 0.3)
+# TEMPERATURES=(0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 0.0 0.1 0.2 0.3)
+TEMPERATURES=(0.1 0.2 0.3)
 
 # Use 3 GPUs (modify these numbers based on your available GPUs)
-CUDA_VISIBLE_DEVICES=0,1
+CUDA_VISIBLE_DEVICES=0,1,2,3
 
-echo "Starting answer generation process for $DATASET dataset using 3 GPUs"
+echo "Starting answer generation process for $DATASET dataset"
 
 export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
@@ -20,7 +21,7 @@ python $SCRIPT_PATH \
     --models $MODEL \
     --dataset_name $DATASET \
     --use_vllm True \
-    --tensor_parallel_size 2 \
+    --tensor_parallel_size 4 \
     --gpu_memory_utilization 0.95
 
 echo "Answer generation completed for $MODEL using GPUs $CUDA_VISIBLE_DEVICES"
